1338654c0f8e5919558b228f9b658b00a952234a
Michael Schreier <mjschreier1@gmail.com>
2017-12-14 13:55:16 -0700

    omitted cors

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express()
 const port = process.env.PORT || 3000
 const fixtures = require('./data/fixtures')
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+// })
 
 app.get('/doughnuts', (req, res, next) => {
   res.send(JSON.stringify(fixtures.doughnuts))
