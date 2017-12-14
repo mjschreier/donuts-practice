975270a0b5fffadcd81f64741b6a1009c37e7a0c
Michael Schreier <mjschreier1@gmail.com>
2017-12-14 16:07:32 -0700

    added cors

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 const fixtures = require('./data/fixtures')
 
+//Added to prevent 405 status
+const cors = require('cors')
+app.use(cors())
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
